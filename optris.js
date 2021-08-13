4f0133e5263c4050bdaa5706004696930b47d49d
filippo.cara <filippo.cara@plasticomnium.com>
2021-08-13 16:43:00 +0200

    Start handling errors

--- optris.js
+++ optris.js
@@ -5,10 +5,11 @@ module.exports = function(RED) {
     function OptrisNode(config) {
         RED.nodes.createNode(this, config);
         var node  = this;
-        node.dllPath = config.dllPath;
-        node.configPath = config.configPath;
-        node.formatPath = config.formatPath;
-        node.imageType = config.imageType;
+        
+        node.dllPath = config.dllPath; // Path to libirimager.ddl file
+        node.configPath = config.configPath; // Path to config file
+        node.formatPath = config.formatPath; // Path to "Formats.def" file
+        node.imageType = config.imageType; // Image Type --> Palette (w, h, 3) or Thermal (w, h)
 
         node.status({fill:"red",shape:"dot",text:"Not recording"});
 
@@ -18,59 +19,69 @@ module.exports = function(RED) {
                 node.status({fill:"yellow",shape:"dot",text:"Connecting"});
 
                 try {
+                    // Try to Load
+                    optris.loadDLL(node.dllPath);
                     // Initialize usb communication
-                    optris.usb_init(node.configPath, node.formatPath)
-                
-                } catch (e) {
-                    node.error(e)
-                }
+                    optris.usb_init(node.configPath, node.formatPath);
 
-                // Compute image sizes
-                if (node.imageType === "thermal_image") {
-                    let w, h = optris.get_thermal_image_size()
-                }
-                else if (node.imageType === "palette_image") {
-                    let w, h = optris.get_palette_image_size()
-                }
-                
-                // Remove garbage images
-                for (let i=0; i < 300; i++) {
+                    // Compute image sizes
                     if (node.imageType === "thermal_image") {
-                        optris.get_thermal_image(w, h)
+                        var w, h = optris.get_thermal_image_size();
                     }
                     else if (node.imageType === "palette_image") {
-                        optris.get_palette_image(w, h)
+                        var w, h = optris.get_palette_image_size();
                     }
-                }
+                    
+                    // Remove garbage images
+                    for (let i=0; i < 300; i++) {
+                        if (node.imageType === "thermal_image") {
+                            optris.get_thermal_image(w, h);
+                        }
+                        else if (node.imageType === "palette_image") {
+                            optris.get_palette_image(w, h);
+                        }
+                    }
 
-                node.status({fill:"green",shape:"dot",text:"Ready"});
+                    node.status({fill:"green",shape:"dot",text:"Ready"});
+
+                } catch (e) {
+                    // If any error during DLL loading or USB initialisation
+                    node.error(e);
+                    node.status({fill:"red",shape:"dot",text:"Error"});
+                }
             }
             // If property reset, then close and release capture object
             else if (msg.hasOwnProperty('reset') === true) {
 
-                    optris.terminate()
+                    optris.terminate();
                     node.status({fill:"red",shape:"dot",text:"Stopped"});
-                 
             }
             // If input and no 'start' or 'reset', then take a shot
             else {
 
-                if (node.imageType === "thermal_image") {
-                    let frame = optris.get_thermal_image(w, h)
-                }
-                else if (node.imageType === "palette_image") {
-                    let frame = optris.get_palette_image(w, h)
-                }
+                try {
 
-                // Prepare payload     
-                msg.payload = {"image":frame, "w":w, "h":h}
-                node.send(msg);
+                    if (node.imageType === "thermal_image") {
+                        var frame = optris.get_thermal_image(w, h);
+                    }
+                    else if (node.imageType === "palette_image") {
+                        var frame = optris.get_palette_image(w, h);
+                    }
+    
+                    // Prepare payload     
+                    msg.payload = {"image":frame, "w":w, "h":h};
+                    node.send(msg);
+
+                } catch (e) {
+                    node.error(e);
+                    node.status({fill:"red",shape:"dot",text:"Error"});
+                }   
             }
         })
         node.on('close', function() {
-            optris.terminate()
+            optris.terminate();
             node.status({fill:"red",shape:"dot",text:"Not recording"});
         })
     }
-    RED.nodes.registerType("optrisnode",OptrisNode);
+    RED.nodes.registerType("optris",OptrisNode);
 };
